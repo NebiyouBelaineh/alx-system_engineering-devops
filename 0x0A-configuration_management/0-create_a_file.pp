3a5d053b7707959d0cadd721d6deedebf12e5137
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-23 22:03:08 +0300

    Fix puppet-lint

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # Script creates a file in /tmp called 'school'
 file { '/tmp/school':
-	ensure => present,
-	content => 'I love Puppet',
-	owner => 'www-data',
-	group => 'www-data',
-	mode => '0744'
+  ensure  => present,
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
 }
