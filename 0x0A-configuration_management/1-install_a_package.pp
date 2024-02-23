979d8a244de63a18624db81f0e51b779850b7fc4
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-23 23:22:01 +0300

    Use exec for installing

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Script that installs flask from pip3
 # Version must be 2.1.0
-package { 'flask':
-  ensure   =>  '2.1.0',
-  provider =>   pip3
-}
-
+exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    path    => ['/usr/bin'],
+    creates => '/usr/local/lib/python3.8/dist-packages/flask', # Adjust the path based on your Python environment
+  }
