f93cc27bab0046df01f6cb69a518afb60ac6cb3e
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-02-29 02:02:03 +0300

    add location

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,46 +1,65 @@
-# install nginx and 
-# return string "Hello World!" when quering the root
-# rediretion must be a "301" move permanentyly
-
+# Update package list
 exec { 'apt-update':
   command     => '/usr/bin/apt-get -y update',
   refreshonly => true,
   before      => Package['nginx'],
 }
 
+# Install Nginx
 package { 'nginx':
   ensure => installed,
 }
 
+# Ensure Nginx service is running
 service { 'nginx':
   ensure  => running,
   enable  => true,
   require => Package['nginx'],
 }
 
+# Define the content for the default server configuration
+$server_config = "
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    server_name _;
+
+    location / {
+        try_files \$uri \$uri/404;
+        return 301 /redirected;
+    }
+
+    location = /redirected {
+        rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
+    }
+
+    location /404 {
+        return 404;
+    }
+}"
+
+# Create the Nginx default server configuration file
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => $server_config,
+  notify  => Service['nginx'],
+}
+
+# Create the Hello World index.html file
 file { '/var/www/html/index.html':
   ensure  => present,
   content => 'Hello World!',
 }
 
-$server_config = "
-	server {
-		listen 80 default_server;
-		listen [::]:80 default_server;
-		
-		root /var/www/html;
-		index index.html;
-
-		server_name _;
-		rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-
-		location / {
-			try_files \$uri \$uri/404;
-		}
-}"
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => $server_config,
-  notify  => Service['nginx'],
+# Ensure Nginx listens on port 80
+file_line { 'listen_on_port_80':
+  ensure  => present,
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'listen 80;',
+  before  => 'server_name _;',
+  require => Package['nginx'],
 }
