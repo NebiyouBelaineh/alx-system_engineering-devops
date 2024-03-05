ddc56523b690ede0987a5f9d3da4d9e511e3383e
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-05 18:10:25 +0300

    Modify puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,45 +1,16 @@
-# Setup New Ubuntu server with nginx
-# and add a custom HTTP header
+# Puppet script that adds custome header to nginx server
 
-exec { 'update system':
+exec {'update':
   command => '/usr/bin/apt-get update',
 }
-
-package { 'nginx':
-  ensure  => 'installed',
-  require => Exec['update system'],
+  package {'nginx':
+  ensure => 'present',
 }
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
+file_line { 'http_header':
+  path  => '/etc/nginx/nginx.conf',
+  match => 'http {',
+  line  => "http {\n\tadd_header X-Served-By \"${HOSTNAME}\";",
 }
-
-exec { 'redirect_me':
-  command  => 'sed -i "53i\\location =  /redirect_me { return 301 http://youtube.com;}" /etc/nginx/sites-enabled/default',
-  provider => 'shell',
-}
-
-exec { 'error_page':
-  command => 'printf "Ceci n\'est pas une page\n" > /usr/share/nginx/html/404.html',
-}
-
-exec { 'error_page_conf':
-  command  => 'sed -i "53i\\    error_page 404 /404.html;\n    location = /404.html {\n      root /usr/share/nginx/html;\n      internal;}"
- /etc/nginx/sites-enabled/default',
-  provider => 'shell',
-}
-
-exec { 'HTTP header':
-  command  => 'sed -i "51i\\     add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-enabled/default',
-  provider => 'shell',
-}
-
-exec { 'HTTP header conf':
-  command  => 'sed -i "29i\\     add_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf',
-  provider => 'shell',
-}
-
-service { 'nginx':
-  ensure  => 'running',
-  require => Package['nginx'],
+exec {'run':
+  command => '/usr/sbin/service nginx restart',
 }
