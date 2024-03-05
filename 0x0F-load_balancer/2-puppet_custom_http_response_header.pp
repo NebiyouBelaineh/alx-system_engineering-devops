1390f5a6aaa400a3bad40564a155ebc0049b8b18
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2024-03-06 01:50:00 +0300

    Add solution for task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,37 @@
 # Puppet script that adds custome header to nginx server
 
-exec {'update':
+
+exec { 'update':
   command => '/usr/bin/apt-get update',
 }
-  package {'nginx':
-  ensure => 'installed',
+
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update'],
 }
-file_line { 'http_header':
-  path   => '/etc/nginx/nginx.conf',
-  line   => "http {\n\tadd_header X-Served-By \$HOSTNAME;",
-  match  => 'http {',
+exec {'nginx_conf_httpp_header':
+  command => 'sudo sed -i "29i\ add_header X-Served-By \$HOSTNAME;" /etc/nginx/nginx.conf',
+  require =>  Package['nginx'],
+  path    => '/bin:/usr/bin',
 }
-exec {'run':
-  command => '/usr/sbin/service nginx stop && /usr/sbin/service nginx start',
-  path    => '/bin:/usr/bin:/usr/sbin',
+
+exec {'nginx_default_http_header':
+  command => 'sudo sed -i "51i\ add_header X-Served-By \$HOSTNAME;" /etc/nginx/sites-enabled/default',
+  require =>  Package['nginx'],
+  path    => '/bin:/usr/bin',
+}
+
+#augeas { 'nginx_http_header':
+#  context => '/files/etc/nginx/nginx.conf',
+#  changes => [
+#  'set http[.]/add_header X-Served-By "$$HOSTNAME";',
+#  ],
+#  require => Package['nginx'],
+#}
+
+service { 'nginx':
+  ensure    => 'running',
+  enable    => true,
+  subscribe => Package['nginx'],
+  require   => Exec['nginx_default_http_header'],
 }
